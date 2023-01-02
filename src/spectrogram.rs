c627dc85d621ccc3942d330e138745252c11a7db
Giles Edkins <edkins@gmail.com>
2023-01-01 19:11:32 -0500

    Fix spectrogram with respect to channels

--- src/spectrogram.rs
+++ src/spectrogram.rs
@@ -10,13 +10,13 @@ pub fn get_spectrogram(info: &ProjectAudio, spec: &ProjectSpectrogram, index: us
     let num_steps = spec.height;
     let num_expected_frequency_bins = spec.width;
     let num_samples = num_steps * spec.samples_per_step + (spec.samples_per_fft - spec.samples_per_step);
-    let start_pos = info.channels * 4 * num_samples * index;
+    let start_pos = info.channels * 4 * num_steps * spec.samples_per_step * index;
     let len = info.channels * 4 * num_samples;
     let mut file = File::open(filename).ok()?;
     file.seek(SeekFrom::Start(start_pos as u64)).ok()?;
     let mut f32_vec:Vec<f32> = vec![0.0; len / 4];
     file.read_f32_into::<LittleEndian>(&mut f32_vec).ok()?;
-    let array = Array::from_vec(f32_vec).into_shape((info.channels, num_samples)).ok()?;
+    let array = Array::from_vec(f32_vec).into_shape((num_samples, info.channels)).ok()?;
 
     let fft = RealFftPlanner::<f32>::new().plan_fft_forward(spec.samples_per_fft);
     let mut fft_input = fft.make_input_vec();
@@ -30,7 +30,7 @@ pub fn get_spectrogram(info: &ProjectAudio, spec: &ProjectSpectrogram, index: us
         for j in 0..num_steps {
             let offset = j * spec.samples_per_step;
             let mut input_view = ArrayViewMut::from_shape((spec.samples_per_fft,), &mut fft_input).expect("ArrayViewMut failure");
-            input_view.assign(&array.slice(s![i, offset..offset+spec.samples_per_fft]));
+            input_view.assign(&array.slice(s![offset..offset+spec.samples_per_fft, i]));
             fft.process(&mut fft_input, &mut fft_output).expect("FFT failed for some reason");
             let output_view = ArrayView::from_shape((num_expected_frequency_bins,), &fft_output).expect("ArrayView failure");
 
