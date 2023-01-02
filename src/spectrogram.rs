9c0f4ac2f5d28cd28ecc5cccf19a319f74688028
Giles Edkins <edkins@gmail.com>
2023-01-02 00:31:23 -0500

    Allow specifying high/low shelf filters

--- src/spectrogram.rs
+++ src/spectrogram.rs
@@ -1,6 +1,5 @@
 use ndarray::{s, Array, ArrayView, ArrayViewMut};
 use realfft::RealFftPlanner;
-use std::cmp::Ordering;
 use std::fs::File;
 use byteorder::{LittleEndian, ReadBytesExt};
 use crate::project::{ProjectAudio, ProjectSpectrogram};
@@ -25,7 +24,6 @@ pub fn get_spectrogram(info: &ProjectAudio, spec: &ProjectSpectrogram) -> Option
     let mut result:Array<u8,_> = Array::zeros((info.channels, spec.height.unwrap(), spec.width, 4));
 
     for i in 0..info.channels {
-        let mut last_sq:Array<f32,_> = Array::zeros((num_freqs,));
         for j in 0..num_steps {
             let offset = j * spec.samples_per_step;
             let mut input_view = ArrayViewMut::from_shape((spec.samples_per_fft,), &mut fft_input).expect("ArrayViewMut failure");
@@ -39,27 +37,12 @@ pub fn get_spectrogram(info: &ProjectAudio, spec: &ProjectSpectrogram) -> Option
             let output_im = output_cx.im.into_owned();
             let output_sq = output_re.clone() * output_re.clone() + output_im.clone() * output_im.clone();
 
-            let mut values_b = vec![0u8; num_freqs];
-            for i in 0..num_freqs {
-                let mut values = vec![0f32; 4];
-                for j in 0..4 {
-                    let k = (i * (j + 1)) / 4;
-                    values[j] = output_sq[k];
-                }
-                values.sort_unstable_by(|a,b|if a<b {Ordering::Less} else {Ordering::Greater});
-                values_b[i] = (values[1].powf(0.33) * 16.) as u8;
-            }
-
             let values_r = output_sq.mapv(|e|(e.powf(0.33) * 16.) as u8);
-            let values_g = values_r.clone();
-            let values_b = Array::from_vec(values_b);
 
             result.slice_mut(s![i, j, .., 0]).assign(&values_r);
-            result.slice_mut(s![i, j, .., 1]).assign(&values_g);
-            result.slice_mut(s![i, j, .., 2]).assign(&values_b);
+            result.slice_mut(s![i, j, .., 1]).assign(&values_r);
+            result.slice_mut(s![i, j, .., 2]).assign(&values_r);
             result.slice_mut(s![i, j, .., 3]).fill(255);
-
-            last_sq.assign(&output_sq);
         }
     }
     Some(result.into_shape((info.channels * spec.height.unwrap() * spec.width * 4,)).expect("into_shape failure").to_vec())
