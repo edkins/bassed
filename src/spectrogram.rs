67031ebdb779a84fa8ab23bd9184c4c1572bd206
Giles Edkins <edkins@gmail.com>
2023-01-02 09:18:17 -0500

    Gaussian window for fft

--- src/spectrogram.rs
+++ src/spectrogram.rs
@@ -1,4 +1,4 @@
-use ndarray::{s, Array, ArrayView, ArrayViewMut};
+use ndarray::{s, Array, ArrayView};
 use realfft::RealFftPlanner;
 use std::fs::File;
 use byteorder::{LittleEndian, ReadBytesExt};
@@ -16,19 +16,25 @@ pub fn get_spectrogram(info: &ProjectAudio, spec: &ProjectSpectrogram) -> Option
     let array = Array::from_vec(f32_vec).into_shape((num_samples, info.channels)).ok()?;
 
     let fft = RealFftPlanner::<f32>::new().plan_fft_forward(spec.samples_per_fft);
-    let mut fft_input = fft.make_input_vec();
     let mut fft_output = fft.make_output_vec();
-    assert_eq!(fft_input.len(), spec.samples_per_fft);
     assert!(fft_output.len() >= num_freqs);
 
-    let mut result:Array<u8,_> = Array::zeros((info.channels, spec.height.unwrap(), spec.width, 4));
+    let wanted_channels = 1;
+    let mut result:Array<u8,_> = Array::zeros((wanted_channels, spec.height.unwrap(), spec.width, 4));
 
-    for i in 0..info.channels {
+    let mut gaussian:Array<f32,_> = Array::zeros((spec.samples_per_fft,));
+    for i in 0..spec.samples_per_fft {
+        let x = ((i as f32) / (spec.samples_per_fft as f32) - 0.5) * 6.0;
+        gaussian[i] = (-x * x).exp();
+    }
+
+    for i in 0..wanted_channels {
         for j in 0..num_steps {
             let offset = j * spec.samples_per_step;
-            let mut input_view = ArrayViewMut::from_shape((spec.samples_per_fft,), &mut fft_input).expect("ArrayViewMut failure");
-            input_view.assign(&array.slice(s![offset..offset+spec.samples_per_fft, i]));
-            fft.process(&mut fft_input, &mut fft_output).expect("FFT failed for some reason");
+
+            let input_array = array.slice(s![offset..offset+spec.samples_per_fft, i]).to_owned();
+            let input_array = input_array.clone() * gaussian.clone();
+            fft.process(&mut input_array.to_vec(), &mut fft_output).expect("FFT failed for some reason");
             let output_view = ArrayView::from_shape((fft_output.len(),), &fft_output).expect("ArrayView failure");
             let output_view = output_view.slice(s![..num_freqs]);
 
@@ -36,14 +42,15 @@ pub fn get_spectrogram(info: &ProjectAudio, spec: &ProjectSpectrogram) -> Option
             let output_re = output_cx.re.into_owned();
             let output_im = output_cx.im.into_owned();
             let output_sq = output_re.clone() * output_re.clone() + output_im.clone() * output_im.clone();
-
-            let values_r = output_sq.mapv(|e|(e.powf(0.33) * 16.) as u8);
+            let values_r = output_sq.mapv(|e|(e.powf(0.25) * 32.) as u8);
+            let values_g = output_sq.mapv(|e|(e.powf(0.25) * 16.) as u8);
+            let values_b = output_sq.mapv(|e|(e.powf(0.25) * 8.) as u8);
 
             result.slice_mut(s![i, j, .., 0]).assign(&values_r);
-            result.slice_mut(s![i, j, .., 1]).assign(&values_r);
-            result.slice_mut(s![i, j, .., 2]).assign(&values_r);
+            result.slice_mut(s![i, j, .., 1]).assign(&values_g);
+            result.slice_mut(s![i, j, .., 2]).assign(&values_b);
             result.slice_mut(s![i, j, .., 3]).fill(255);
         }
     }
-    Some(result.into_shape((info.channels * spec.height.unwrap() * spec.width * 4,)).expect("into_shape failure").to_vec())
+    Some(result.into_shape((wanted_channels * spec.height.unwrap() * spec.width * 4,)).expect("into_shape failure").to_vec())
 }
